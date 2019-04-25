86c6539e213f19ec750dd4edce0f919032b4d29b
Shilpa <silpa.isha@gmail.com>
2019-04-25 18:12:00 -0500

    Create ruby.rb
    Added ruby file

---
+++ ruby.rb
@@ -0,0 +1,55 @@
+Last login: Mon Feb 19 12:03:40 on ttys001
+suhass-mbp:~ spinnaka$ cd ~/desktop/vagrant
+suhass-mbp:vagrant spinnaka$ vagrant up
+Bringing machine 'default' up with 'virtualbox' provider...
+==> default: Machine already provisioned. Run `vagrant provision` or use the `--provision`
+==> default: flag to force provisioning. Provisioners marked to run always will still run.
+suhass-mbp:vagrant spinnaka$ vagrant ssh
+Welcome to Ubuntu 14.04.3 LTS (GNU/Linux 3.13.0-76-generic x86_64)
+
+ * Documentation:  https://help.ubuntu.com/
+
+  System information as of Mon Feb 19 18:02:05 UTC 2018
+
+  System load:  0.08              Processes:           84
+  Usage of /:   6.9% of 39.34GB   Users logged in:     1
+  Memory usage: 23%               IP address for eth0: 10.0.2.15
+  Swap usage:   0%
+
+  Graph this data and manage this system at:
+    https://landscape.canonical.com/
+
+  Get cloud support with Ubuntu Advantage Cloud Guest:
+    http://www.ubuntu.com/business/services/cloud
+
+New release '16.04.3 LTS' available.
+Run 'do-release-upgrade' to upgrade to it.
+
+
+Last login: Mon Feb 19 18:02:06 2018 from 10.0.2.2
+Now using node v7.10.0 (npm v4.2.0)
+
+Welcome to your Firehose Web Dev Environment
+For more info check out theFirehoseProject.com
+
+[Web Dev]: /vagrant $ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
